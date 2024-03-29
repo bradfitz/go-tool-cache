2313b1d20a04f3823ac49a5bab6616563ad0c019
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2024-03-28 17:18:43 -0700

    restore disk cache

--- cmd/go-cacher-s3/cacher.go
+++ cmd/go-cacher-s3/cacher.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -21,10 +22,14 @@ import (
 
 const defaultCacheKey = "v1"
 
+var defaultLocalCacheDir, _ = os.UserCacheDir()
+
 var (
-	flagVerbose  = flag.Bool("verbose", false, "be verbose")
-	flagCacheKey = flag.String("cache-key", defaultCacheKey, "cache key")
-	bucket       string
+	flagVerbose       = flag.Bool("verbose", false, "be verbose")
+	flagDebug         = flag.Bool("debug", false, "dump a lot of debug info")
+	flagCacheKey      = flag.String("cache-key", defaultCacheKey, "cache key")
+	flagLocalCacheDir = flag.String("local-cache-dir", defaultLocalCacheDir, "local cache directory")
+	bucket            string
 )
 
 func main() {
@@ -38,12 +43,17 @@ func main() {
 	defer cancel()
 
 	log.Printf("starting cache")
-	awsConfig, err := config.LoadDefaultConfig(context.TODO())
+	var clientLogMode aws.ClientLogMode
+	if flagDebug != nil && *flagDebug {
+		clientLogMode = aws.LogRetries | aws.LogRequest
+	}
+	awsConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithClientLogMode(clientLogMode))
 	if err != nil {
 		log.Fatal("S3 cache disabled; failed to load AWS config: ", err)
 	}
 	proc := cacheproc.NewCacheProc(
-		cachers.NewCombinedCache(&cachers.NoopLocalCache{},
+		cachers.NewCombinedCache(
+			cachers.NewSimpleDiskCache(*flagVerbose, *flagLocalCacheDir),
 			cachers.NewS3Cache(s3.NewFromConfig(awsConfig), bucket, *flagCacheKey, *flagVerbose),
 			*flagVerbose,
 		),
