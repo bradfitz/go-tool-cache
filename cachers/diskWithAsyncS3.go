f37c4ff2d1cfe9d5964f4746153f74c6636d58f9
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2024-04-05 12:09:11 -0700

    pull s3 inline

--- cachers/diskWithAsyncS3.go
+++ cachers/diskWithAsyncS3.go
@@ -8,7 +8,11 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
 )
 
 type putWork struct {
@@ -19,24 +23,30 @@ type putWork struct {
 }
 
 type DiskAsyncS3Cache struct {
-	log            *slog.Logger
-	localCache     *LocalCacheWithCounts
-	s3Client       s3Client
-	bucketName     string
-	cacheKey       string
-	remoteWork     chan putWork
-	remoteWG       *sync.WaitGroup
-	remoteGetCache *RemoteCacheWithCounts
-	putsMetrics    *timeKeeper
-	getsMetrics    *timeKeeper
-	nWorkers       int
+	log         *slog.Logger
+	localCache  *LocalCacheWithCounts
+	s3Client    s3Client
+	bucketName  string
+	s3Prefix    string
+	remoteWork  chan putWork
+	remoteWG    *sync.WaitGroup
+	putsMetrics *timeKeeper
+	getsMetrics *timeKeeper
+	nWorkers    int
 }
 
 var _ LocalCache = &DiskAsyncS3Cache{}
 
-func NewDiskAsyncS3Cache(localCache LocalCache, client s3Client, bucketName string, cacheKey string, queueLen int, nWorkers int) LocalCache {
-	remoteGet := NewRemoteCacheStats(NewS3Cache(client, bucketName, cacheKey))
-	remoteGet.Name = "remoteGet"
+const (
+	outputIDMetadataKey = "outputid"
+)
+
+type s3Client interface {
+	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+}
+
+func NewDiskAsyncS3Cache(localCache LocalCache, client s3Client, bucketName string, s3Prefix string, queueLen int, nWorkers int) LocalCache {
 	return NewLocalCacheStats(&DiskAsyncS3Cache{
 		log:        slog.Default().WithGroup("DiskAsyncS3"),
 		remoteWork: make(chan putWork, queueLen),
@@ -44,13 +54,13 @@ func NewDiskAsyncS3Cache(localCache LocalCache, client s3Client, bucketName stri
 		nWorkers:   nWorkers,
 		s3Client:   client,
 		bucketName: bucketName,
-		cacheKey:   cacheKey,
+		// NOTE: previous incarnations contained the GOARCH and GOOS in the cache key, but I'm pretty sure they need not be separate
+		s3Prefix: s3Prefix,
 		// note: we initialize remoteWG in Start
 		// TODO: instead of making wrappers, just integrate Counts with the real things
-		localCache:     NewLocalCacheStats(localCache),
-		remoteGetCache: remoteGet,
-		putsMetrics:    newTimeKeeper(),
-		getsMetrics:    newTimeKeeper(),
+		localCache:  NewLocalCacheStats(localCache),
+		putsMetrics: newTimeKeeper(),
+		getsMetrics: newTimeKeeper(),
 	})
 }
 
@@ -60,29 +70,20 @@ func (c *DiskAsyncS3Cache) Start(ctx context.Context) error {
 		return fmt.Errorf("local cache start failed: %w", err)
 	}
 
-	err = c.remoteGetCache.Start(ctx)
-	if err != nil {
-		c.localCache.Close()
-		return fmt.Errorf("remote cache start failed: %w", err)
-	}
-
 	c.log.Info("probing remote cache")
 	probeStr := "_probe"
-	err = c.remoteGetCache.Put(ctx, probeStr, probeStr, int64(len([]byte(probeStr))), bytes.NewReader([]byte(probeStr)))
+	err = c.s3Put(ctx, probeStr, probeStr, int64(len([]byte(probeStr))), bytes.NewReader([]byte(probeStr)))
 	if err != nil {
 		c.localCache.Close()
-		c.remoteGetCache.Close()
 		return fmt.Errorf("remote cache probe put failed: %w", err)
 	}
-	_, sz, _, err := c.remoteGetCache.Get(ctx, probeStr)
+	_, sz, _, err := c.s3Get(ctx, probeStr)
 	if err != nil {
 		c.localCache.Close()
-		c.remoteGetCache.Close()
 		return fmt.Errorf("remote cache probe get failed: %w", err)
 	}
 	if sz != int64(len([]byte(probeStr))) {
 		c.localCache.Close()
-		c.remoteGetCache.Close()
 		return fmt.Errorf("remote cache probe get size mismatch: expected %d, got %d", len([]byte(probeStr)), sz)
 	}
 	c.log.Info("probe success")
@@ -91,22 +92,14 @@ func (c *DiskAsyncS3Cache) Start(ctx context.Context) error {
 	for i := 0; i < c.nWorkers; i++ {
 		go func() {
 			defer c.remoteWG.Done()
-			rc := NewRemoteCacheStats(NewS3Cache(c.s3Client, c.bucketName, c.cacheKey))
-			rc.Name = "remoteWorker"
-			err := rc.Start(ctx)
-			if err != nil {
-				c.log.Error("remote worker start failed", "err", err)
-				return
-			}
-			defer rc.Close()
 			for {
 				select {
 				case w, ok := <-c.remoteWork:
 					if !ok {
-						c.log.Info("remote worker done by closed work channel")
+						c.log.Debug("s3 worker done by closed work channel")
 						return
 					}
-					c.log.Debug("remote worker put", "actionID", w.actionID, "outputID", w.outputID, "size", w.size, "diskPath", w.diskPath)
+					c.log.Debug("s3 put", "actionID", w.actionID, "outputID", w.outputID, "size", w.size, "diskPath", w.diskPath)
 					var r io.Reader
 					if w.size == 0 {
 						r = bytes.NewReader(nil)
@@ -121,13 +114,13 @@ func (c *DiskAsyncS3Cache) Start(ctx context.Context) error {
 						r = f
 					}
 					// TODO: not 100% on the lifetime of this context; is it until everything is started? or until Close? we may want a separate Context for workers so that they can be stopped before all work is done (i.e., on Close)
-					err := rc.Put(ctx, w.actionID, w.outputID, w.size, r)
+					err := c.s3Put(ctx, w.actionID, w.outputID, w.size, r)
 					if err != nil {
 						c.log.Error("putting to remote", "actionID", w.actionID, "outputID", w.outputID, "err", err)
 						continue
 					}
 				case <-ctx.Done():
-					c.log.Info("remote worker done by ctx.Done")
+					c.log.Debug("s3 worker done by ctx.Done")
 					return
 				}
 			}
@@ -139,13 +132,52 @@ func (c *DiskAsyncS3Cache) Start(ctx context.Context) error {
 	return nil
 }
 
+func (c *DiskAsyncS3Cache) s3Put(ctx context.Context, actionID, outputID string, size int64, body io.Reader) error {
+	if size == 0 {
+		body = bytes.NewReader(nil)
+	}
+	c.log.Debug("s3 put", "actionID", actionID, "outputID", outputID, "size", size)
+	actionKey := c.actionKey(actionID)
+	_, err := c.s3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:        &c.bucketName,
+		Key:           &actionKey,
+		Body:          body,
+		ContentLength: &size,
+		Metadata: map[string]string{
+			outputIDMetadataKey: outputID,
+		},
+	})
+	return err
+}
+
+func (c *DiskAsyncS3Cache) s3Get(ctx context.Context, actionID string) (outputID string, size int64, output io.ReadCloser, err error) {
+	c.log.Debug("s3 get", "actionID", actionID)
+	actionKey := c.actionKey(actionID)
+	outputResult, getOutputErr := c.s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &c.bucketName,
+		Key:    &actionKey,
+	})
+	if isS3NotFoundError(getOutputErr) {
+		// TODO: count miss
+		return "", 0, nil, nil
+	} else if getOutputErr != nil {
+		return "", 0, nil, fmt.Errorf("unexpected S3 get for %s:  %v", actionKey, getOutputErr)
+	}
+	contentSize := outputResult.ContentLength
+	outputID, ok := outputResult.Metadata[outputIDMetadataKey]
+	if !ok || outputID == "" {
+		return "", 0, nil, fmt.Errorf("outputId not found in metadata")
+	}
+	return outputID, *contentSize, outputResult.Body, nil
+}
+
 func (c *DiskAsyncS3Cache) Get(ctx context.Context, actionID string) (string, string, error) {
 	c.log.Debug("get", "actionID", actionID)
 	outputID, diskPath, err := c.localCache.Get(ctx, actionID)
 	if err == nil && outputID != "" {
 		return outputID, diskPath, nil
 	}
-	outputID, size, output, err := c.remoteGetCache.Get(ctx, actionID)
+	outputID, size, output, err := c.s3Get(ctx, actionID)
 	if err != nil {
 		return "", "", err
 	}
@@ -162,6 +194,7 @@ func (c *DiskAsyncS3Cache) Get(ctx context.Context, actionID string) (string, st
 	return outputID, diskPath, nil
 }
 
+// TODO: there's a problem when the disk and s3 get out of sync: if the disk has a file that the s3 doesn't, it will never get put to s3. This is maybe fine, since eventually the disk cache will be cleared?
 func (c *DiskAsyncS3Cache) Put(ctx context.Context, actionID, outputID string, size int64, body io.Reader) (string, error) {
 	c.log.Debug("put", "actionID", actionID, "outputID", outputID, "size", size)
 	// special case for empty files, nead empty reader
@@ -192,9 +225,6 @@ func (c *DiskAsyncS3Cache) Close() error {
 	// TODO: this means we wait till all the remote workers finish; we may want to just abandon the rest of the work (or offer a mode)
 	close(c.remoteWork)
 	c.remoteWG.Wait()
-	if err := c.remoteGetCache.Close(); err != nil {
-		errAll = errors.Join(fmt.Errorf("remote get cache stop failed: %w", err), errAll)
-	}
 	if err := c.putsMetrics.Stop(); err != nil {
 		errAll = errors.Join(fmt.Errorf("puts metrics stop failed: %w", err), errAll)
 	}
@@ -204,4 +234,26 @@ func (c *DiskAsyncS3Cache) Close() error {
 	// TODO: pull out the metrics into log KV
 	c.log.Info(fmt.Sprintf("Downloads: %s, Uploads %s", c.getsMetrics.Summary(), c.putsMetrics.Summary()))
 	return errAll
+}
+
+func (c *DiskAsyncS3Cache) actionKey(actionID string) string {
+	return fmt.Sprintf("%s/%s", c.s3Prefix, actionID)
+}
+
+func isS3NotFoundError(err error) bool {
+	if err != nil {
+		var ae smithy.APIError
+		if errors.As(err, &ae) {
+			code := ae.ErrorCode()
+			if code == "NoSuchKey" {
+				return true
+			}
+			if code == "AccessDenied" {
+				// technically if sig doesn't match, it is unknown whether found or not
+				return !strings.Contains(ae.Error(), "SignatureDoesNotMatch")
+			}
+			return false
+		}
+	}
+	return false
 }
